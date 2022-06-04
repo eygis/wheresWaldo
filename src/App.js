fb6f3ce01213a4067ffef142a842a517422e72d6
eygis <ahill033@yahoo.com>
2022-06-04 09:42:21 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './App.css'
+const App = () => {
+    
+    return (
+        <div id='wrapper'>
+         <h3>test</h3>
+        </div>
+    );
+};
+
+export default App;
